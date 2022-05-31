cdc1eecb03666209b27fa478a521e56ab113bd4b
Jen Moriarty <JMoriarty78@gmail.com>
2022-05-31 11:30:36 -0600

    Create step2.py

---
+++ step2.py
@@ -0,0 +1,23 @@
+import pygame  #attaches pygame module to program
+pygame.init()  #initializes pygame
+pygame.display.set_caption("my game")  # sets the window title
+screen = pygame.display.set_mode((200,200))  # creates game screen
+screen.fill((0,250,0)) #sets background color of game screen
+
+#game variables
+gameOver = False
+
+#OMG GAME LOOP###################################################
+while gameOver == False:
+    #Input section-----------------------------------------
+    
+    #physics update section--------------------------------
+    
+    #render section----------------------------------------
+    pygame.display.flip()#draws everything to the screen
+    
+#END OF GAME LOOP################################################
+pygame.quit()
+
+
+
