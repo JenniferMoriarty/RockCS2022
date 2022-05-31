b2dd240f73dea7699865836b5d1238e7ecef64aa
Jen Moriarty <JMoriarty78@gmail.com>
2022-05-31 13:45:13 -0600

    Update step1.2.py

--- step1.2.py
+++ step1.2.py
@@ -1,6 +1,6 @@
 import pygame  #attaches pygame module to program
 pygame.init()  #initializes pygame
-pygame.display.set_caption("my game")  # sets the window title
-screen = pygame.display.set_mode((200,200))  # creates game screen
-screen.fill((0,250,0)) #sets background color of game screen
+pygame.display.set_caption("Platformer")  # sets the window title
+screen = pygame.display.set_mode((500,500))  # creates game screen
+screen.fill((0,0,50)) #sets background color of game screen
 pygame.display.flip()#draws everything to the screen
