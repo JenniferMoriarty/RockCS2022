a7227750877c63440a2730503de470c05e349db2
Jen Moriarty <JMoriarty78@gmail.com>
2022-05-31 19:27:46 -0600

    Create step4.py

---
+++ step4.py
@@ -0,0 +1,57 @@
+import pygame  #attaches pygame module to program
+pygame.init()  #initializes pygame
+pygame.display.set_caption("Platformer")  # sets the window title
+screen = pygame.display.set_mode((500,500))  # creates game screen
+screen.fill((0,0,50)) #sets background color of game screen
+
+#game variables
+gameOver = False #Boolean variable to run game loop
+clock = pygame.time.Clock() #set up clock
+
+#player variables
+xpos = 100 #xposition of player
+ypos = 100 #yposition of player
+vx = 0 #x velocity of player
+vy = 0 #y velocity of player
+keys = [False, False, False, False] #this list holds whether each key has been pressed, slots are 0-3
+
+#OMG GAME LOOP###################################################
+while gameOver == False:
+    
+    #Input section-----------------------------------------
+    clock.tick(60) #FPS (frames per second, aka game speed)
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: #quit game if x is pressed in top corner
+            gameOver = True
+      
+        if event.type == pygame.KEYDOWN: #key has been pressed down
+            if event.key == pygame.K_LEFT: #check if left arrow has been pressed
+                keys[0] = True #if so, change list slot value
+
+        elif event.type == pygame.KEYUP: #key has been released
+            if event.key == pygame.K_LEFT: #check if left arrow has been released
+                keys[0] = False #if so, change list slot value
+
+    
+    #physics update section--------------------------------
+    
+    #LEFT MOVEMENT
+    if keys[0] == True: #check list value
+        vx = -3 #set velocity
+    else:
+        vx = 0
+        
+    #update player position by adding velocity to position
+    xpos += vx 
+    ypos += vy
+    
+    #render section----------------------------------------
+    screen.fill((0,0,50)) #wipe screen so it doesn't smear
+    
+    pygame.draw.rect(screen, (200,0,0), (xpos, ypos, 20, 20)) #draw player
+    
+    pygame.display.flip()#draws everything to the screen
+    
+#END OF GAME LOOP################################################
+pygame.quit()
+
